b0b8583b9a751e575fd5ef40cc840c2fbe2bc5e6
FraSkoll <fcodega2@gmail.com>
2019-12-12 17:14:30 +0100

    completed the ECB rates extractor function

--- utils/data_download.py
+++ utils/data_download.py
@@ -44,37 +44,40 @@ def CW_extractor(exchange_array,currencypair_array,periods):
 			    print("done..." +before)
 			    i += 1
     
-
-# Key (dimensions) explained:
-# the frequency of the measurement: D for daily
-# the currency being measured: CHF for Swiss Francs
-# the currency against which a currency is being measured: EUR for Euros
-# the type of exchange rates: foreign exchange reference rates have code SP00
-# the series variation (such as average or standardised measure for given frequency): code A
-
+#function that downloads the exchange rates from the ECB web page and returns a matrix (pd.DataFrame) that indicates:
+#on the first column the date, on the second tha exchange rate vakue eutro based, on the third the currency, on the fourth the
+#currency of denomination (always 'EUR') and on the fifth the exchange rate USD based (the one of interest)
+# key_currency_vector expects a list of currency in International Currency Formatting (ex. USD, GBP, JPY, CAD,...)
+# the functions diplays the information better for a single day data retrival, however can works with multiple date
+# regarding the other default variables consult the ECB api web page
 def ECB_rates_extractor(key_currency_vector,Start_Period,End_Period=None,frequency='D', currency_denominat='EUR',typeRates='SP00',series_variation='A'):
     if End_Period == None:
         End_Period=Start_Period
     entrypoint = 'https://sdw-wsrest.ecb.europa.eu/service/' 
     resource = 'data'           
-    flowRef ='EXR'              # Dataflow describing the data that needs to be returned, exchange rates in this case
+    flowRef ='EXR'
     parameters = {
-        'startPeriod': Start_Period,  # Start date of the time series
-        'endPeriod': End_Period    # End of the time series
+        'startPeriod': Start_Period, 
+        'endPeriod': End_Period    
     }
     Exchange_Rate_List=pd.DataFrame()
-    for currency in key_currency_vector:
+    pd.options.mode.chained_assignment = None
+    for i,currency in enumerate(key_currency_vector):
         key= frequency+'.'+currency+'.'+currency_denominat+'.'+typeRates+'.'+series_variation
         request_url = entrypoint + resource + '/'+ flowRef + '/' + key
         response = requests.get(request_url, params=parameters, headers={'Accept': 'text/csv'})
         Data_Frame = pd.read_csv(io.StringIO(response.text))
         Main_Data_Frame = Data_Frame.filter(['TIME_PERIOD', 'OBS_VALUE','CURRENCY','CURRENCY_DENOM'], axis=1)
+        if currency == 'USD':
+            cambio_USD_EUR=float(Main_Data_Frame['OBS_VALUE'])
         # 'TIME_PERIOD' was of type 'object' (as seen in Data_Frame.info). Convert it to datetime first
         Main_Data_Frame['TIME_PERIOD'] = pd.to_datetime(Main_Data_Frame['TIME_PERIOD'])
         # Set 'TIME_PERIOD' to be the index
         Main_Data_Frame = Main_Data_Frame.set_index('TIME_PERIOD')
-        if Exchange_Rate_List.size == []:
+        if Exchange_Rate_List.size==0:
             Exchange_Rate_List=Main_Data_Frame
+            Exchange_Rate_List['USD based rate']=float(Main_Data_Frame['OBS_VALUE'])/cambio_USD_EUR
         else:
-            Exchange_Rate_List.append(Main_Data_Frame,ignore_index=True)
-    return Exchange_Rate_List
+            Exchange_Rate_List=Exchange_Rate_List.append(Main_Data_Frame, sort=True)
+            Exchange_Rate_List['USD based rate'][i]=float(Main_Data_Frame['OBS_VALUE'])/cambio_USD_EUR
+        return Exchange_Rate_List
