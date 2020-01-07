2aefc1a4478d0c58ee341b7caca42e6d02817977
Paolo Mazzocchi <mazzocchip@live.it>
2020-01-07 16:32:59 +0100

    Assorted fixes

--- utils/data_download.py
+++ utils/data_download.py
@@ -12,23 +12,26 @@ import numpy as np
 # the function creates exchange_arrayXcurrencypair json files with the info retrieved from the websites in the
 # date range specified by start_date and end_date (end_date default is today())
 # the default frequency is daily (86400 seconds)
-# start_date ed end_date has to be inserted in MM-DD-YYYY format		
-def CW_retrive_json(exchange_array,currencypair_array,start_date, end_date = None, periods='86400'):
-    if "/" in start_date:
-        start_date=start_date.replace("/","-") 
-    start_date = datetime.strptime(start_date, '%m-%d-%Y')
-    if end_date == None:
-        end_date=datetime.now().strftime('%m-%d-%Y')
-    end_date = datetime.strptime(end_date, '%m-%d-%Y')
-    start_date=str(int(time.mktime(start_date.timetuple())))
-    end_date=str(int(time.mktime(end_date.timetuple())))
-    result = ""
-    for exchange in exchange_array :
-        for cp in currencypair_array:
-            request_URL = "https://api.cryptowat.ch/markets/"+exchange+"/"+cp+"/ohlc?periods="+periods+"&after="+start_date+"&before="+end_date
-            result = get(request_URL).content.decode()
-            file_json = open(""+exchange+"_"+cp+".json", "w")
-            file_json.write(result)
+# start_date ed end_date has to be inserted in MM-DD-YYYY format
+
+# def CW_retrive_json(exchange_array,currencypair_array,start_date, end_date = None, periods='86400'):
+#     if "/" in start_date:
+#         start_date=start_date.replace("/","-") 
+#     start_date = datetime.strptime(start_date, '%m-%d-%Y')
+#     if end_date == None:
+#         end_date=datetime.now().strftime('%m-%d-%Y')
+#     end_date = datetime.strptime(end_date, '%m-%d-%Y')
+#     start_date=str(int(time.mktime(start_date.timetuple())))
+#     end_date=str(int(time.mktime(end_date.timetuple())))
+#     result = ""
+#     for exchange in exchange_array :
+#         for cp in currencypair_array:
+#             request_URL = "https://api.cryptowat.ch/markets/"+exchange+"/"+cp+"/ohlc?periods="+periods+"&after="+start_date+"&before="+end_date
+#             result = get(request_URL).content.decode()
+#             file_json = open(""+exchange+"_"+cp+".json", "w")
+#             file_json.write(result)
+
+
 
 # function that retrives data from th Cryptowatch websites and returns a Data Frame with the following
 # headers ['Time' ,'Open',	'High',	'Low',	'Close Price',Crypto+ " Volume" , Pair+" Volume"]
@@ -36,67 +39,107 @@ def CW_retrive_json(exchange_array,currencypair_array,start_date, end_date = Non
 # date range specified by start_date and end_date (end_date default is today())
 # the default frequency is daily (86400 seconds)
 # start_date ed end_date has to be inserted in MM-DD-YYYY format
-def CW_data_reader(exchange,currencypair,start_date, end_date = None, periods='86400'):
-    Crypto=currencypair[:3].upper()
-    Pair=currencypair[3:].upper()
+
+# TODO: check date format without separator
+# TODO: check end date format
+
+def CW_data_reader(exchange, currencypair, start_date, end_date = None, periods='86400'):
+
+    Crypto = currencypair[:3].upper()
+    Pair = currencypair[3:].upper()
+    
+    # check date format
     if "/" in start_date:
-        start_date=start_date.replace("/","-") 
+        start_date = start_date.replace("/","-") 
     start_date = datetime.strptime(start_date, '%m-%d-%Y')
+
+    # set end_date = today if empty
     if end_date == None:
-        end_date=datetime.now().strftime('%m-%d-%Y')
+        end_date = datetime.now().strftime('%m-%d-%Y')
+
     end_date = datetime.strptime(end_date, '%m-%d-%Y')
-    start_date=str(int(time.mktime(start_date.timetuple())))
-    end_date=str(int(time.mktime(end_date.timetuple())))
+
+    # transform date into timestamps
+    start_date = str(int(time.mktime(start_date.timetuple())))
+    end_date = str(int(time.mktime(end_date.timetuple())))
+
+    # API settings
     entrypoint = 'https://api.cryptowat.ch/markets/' 
-    key=exchange+"/"+currencypair+"/ohlc?periods="+periods+"&after="+start_date+"&before="+end_date
-    pd.options.mode.chained_assignment = None
+    key = exchange + "/" + currencypair + "/ohlc?periods=" + periods + "&after=" + start_date + "&before=" + end_date
     request_url = entrypoint + key
+    
+    # API call
     response = requests.get(request_url)
-    response= response.json()
-    header=['Time' ,'Open',	'High',	'Low',	'Close Price',Crypto+" Volume" , Pair+" Volume"]
-    Data_Frame=pd.DataFrame(response['result']['86400'],columns=header)
+    response = response.json()
+    header = ['Time', 'Open', 'High', 'Low', 'Close Price', Crypto + " Volume", Pair + " Volume"]
+    
+    # do not show unuseful messages
+    pd.options.mode.chained_assignment = None
+    
+    Data_Frame = pd.DataFrame(response['result']['86400'], columns = header)
+    
     return Data_Frame 
 
-#function that downloads the exchange rates from the ECB web page and returns a matrix (pd.DataFrame) that indicates:
-#on the first column the date, on the second tha exchange rate vakue eutro based, on the third the currency, on the fourth the
-#currency of denomination (always 'EUR') and on the fifth the exchange rate USD based (the one of interest)
-# key_currency_vector expects a list of currency in International Currency Formatting (ex. USD, GBP, JPY, CAD,...)
+
+
+# function that downloads the exchange rates from the ECB web page and returns a matrix (pd.DataFrame) that indicates:
+# on the first column the date, on the second tha exchange rate vakue eutro based, on the third the currency, on the fourth the
+# currency of denomination (always 'EUR') and on the fifth the exchange rate USD based (the one of interest)
+# key_curr_vector expects a list of currency in International Currency Formatting (ex. USD, GBP, JPY, CAD,...)
 # the functions diplays the information better for a single day data retrival, however can works with multiple date
 # regarding the other default variables consult the ECB api web page
 # Start_Period has to be in YYYY-MM-DD format
-def ECB_rates_extractor(key_currency_vector,Start_Period,End_Period=None,frequency='D', currency_denominat='EUR',typeRates='SP00',series_variation='A'):
+
+def ECB_rates_extractor(key_curr_vector, Start_Period, End_Period = None, freq = 'D', 
+                        curr_den = 'EUR', type_rates = 'SP00', series_var = 'A'):
+    
+    # set end_period = start_period if empty
     if End_Period == None:
-        End_Period=Start_Period
+        End_Period = Start_Period
+
+    # API settings
     entrypoint = 'https://sdw-wsrest.ecb.europa.eu/service/' 
     resource = 'data'           
-    flowRef ='EXR'
-    parameters = {
+    flow_ref = 'EXR'
+    param = {
         'startPeriod': Start_Period, 
         'endPeriod': End_Period    
     }
-    Exchange_Rate_List=pd.DataFrame()
+
+    Exchange_Rate_List = pd.DataFrame()
     pd.options.mode.chained_assignment = None
-    for i,currency in enumerate(key_currency_vector):
-        key= frequency+'.'+currency+'.'+currency_denominat+'.'+typeRates+'.'+series_variation
-        request_url = entrypoint + resource + '/'+ flowRef + '/' + key
-        response = requests.get(request_url, params=parameters, headers={'Accept': 'text/csv'})
+
+    for i, currency in enumerate(key_curr_vector):
+        key = freq + '.' + currency + '.' + curr_den + '.' + type_rates + '.' + series_var
+        request_url = entrypoint + resource + '/' + flow_ref + '/' + key
+        
+        # API call
+        response = requests.get(request_url, params = param, headers = {'Accept': 'text/csv'})
+        
+        # if data is empty, it is an holiday, therefore exit
         try:
             Data_Frame = pd.read_csv(io.StringIO(response.text))
         except:
             break
-        Main_Data_Frame = Data_Frame.filter(['TIME_PERIOD', 'OBS_VALUE','CURRENCY','CURRENCY_DENOM'], axis=1)
+        
+        Main_Data_Frame = Data_Frame.filter(['TIME_PERIOD', 'OBS_VALUE', 'CURRENCY', 'CURRENCY_DENOM'], axis=1)
+
         if currency == 'USD':
-            cambio_USD_EUR=float(Main_Data_Frame['OBS_VALUE'])
+            cambio_USD_EUR = float(Main_Data_Frame['OBS_VALUE'])
+
         # 'TIME_PERIOD' was of type 'object' (as seen in Data_Frame.info). Convert it to datetime first
         Main_Data_Frame['TIME_PERIOD'] = pd.to_datetime(Main_Data_Frame['TIME_PERIOD'])
+        
         # Set 'TIME_PERIOD' to be the index
         Main_Data_Frame = Main_Data_Frame.set_index('TIME_PERIOD')
-        if Exchange_Rate_List.size==0:
-            Exchange_Rate_List=Main_Data_Frame
-            Exchange_Rate_List['USD based rate']=float(Main_Data_Frame['OBS_VALUE'])/cambio_USD_EUR
+        
+        if Exchange_Rate_List.size == 0:
+            Exchange_Rate_List = Main_Data_Frame
+            Exchange_Rate_List['USD based rate'] = float(Main_Data_Frame['OBS_VALUE']) / cambio_USD_EUR
         else:
-            Exchange_Rate_List=Exchange_Rate_List.append(Main_Data_Frame, sort=True)
-            Exchange_Rate_List['USD based rate'][i]=float(Main_Data_Frame['OBS_VALUE'])/cambio_USD_EUR
+            Exchange_Rate_List = Exchange_Rate_List.append(Main_Data_Frame, sort=True)
+            Exchange_Rate_List['USD based rate'][i] = float(Main_Data_Frame['OBS_VALUE']) / cambio_USD_EUR
+
     return Exchange_Rate_List
 
 
--- utils/data_setup.py
+++ utils/data_setup.py
@@ -7,83 +7,105 @@ from datetime import datetime
 from datetime import *
 import time
 
-#function that generate an array of date starting from start_date to end_date
+
+
+# function that generate an array of date starting from start_date to end_date
 # if not specified end_date = today() 
 # default format is in second since the epoch, type timeST='N' for date in format YY-mm-dd
-#write all date in MM/DD/YYYY format
-def date_array_gen(start_date, end_date ='',timeST='Y'):
-    if end_date == '':
-        today=datetime.now().strftime('%m-%d-%Y')
-        dateIndex=pd.date_range(start_date,today)
-    else:
-        dateIndex=pd.date_range(start_date,end_date)
-    DateTSlist=[]
-    DateList=[]
-    for date in dateIndex:
-        DateTSlist.append(str(int(time.mktime(date.timetuple()))))
-    if timeST=='N':
-        for string in DateTSlist:
-            value=int(string)
-            DateList.append(datetime.utcfromtimestamp(value).strftime('%Y-%m-%d'))
-        return DateList
-    return DateTSlist
+# write all date in MM/DD/YYYY format
 
-#given a start date and a period (number of days) the function returns an array containing
-#the "period" date going back from the start date (default) or starting from the start date (direction='forward')
-#the output can be both in timestamp since epoch (default) or in date MM/DD/YYYY (timeST='N')
+def date_array_gen(start_date, end_date = '', timeST = 'Y'):
+
+    # set end_date = today if empty
+    if end_date == '':
+        end_date = datetime.now().strftime('%m-%d-%Y')
+
+    date_index = pd.date_range(start_date, end_date)
+    
+    DateList = date_list(date_index, timeST)
+    
+    return DateList
+
+
+
+
+# given a start date and a period (number of days) the function returns an array containing
+# the "period" date going back from the start date (default) or starting from the start date 
+# (direction='forward') the output can be both in timestamp since epoch (default) or in date 
+# MM/DD/YYYY (timeST='N')
+
 def period_array(start_date, period, direction = 'backward', timeST='Y'):
+
+    # TODO: create a new function to check date format
     if "/" in start_date:
         start_date=start_date.replace("/","-")  
-    if direction=='forward':
-        end_date=datetime.strptime(start_date,'%m-%d-%Y')+ timedelta(days=period) 
-        dateIndex=pd.date_range(start_date,end_date)
+
+    if direction == 'backward':
+        end_date = datetime.strptime(start_date,'%m-%d-%Y') - timedelta(days = period) 
+        date_index = pd.date_range(end_date, start_date)
+
     else:
-        end_date=datetime.strptime(start_date,'%m-%d-%Y')- timedelta(days=period) 
-        dateIndex = pd.date_range(end_date,start_date)
-    DateTSlist=[]
+        end_date = datetime.strptime(start_date,'%m-%d-%Y') + timedelta(days = period) 
+        date_index = pd.date_range(start_date, end_date)
+    
+    DateList = date_list(date_index, timeST)
+    
+    return DateList
+
+
+
+# TODO: add description 
+def date_list(date_index, timeST = 'Y'):
+    
     DateList=[]
-    for date in dateIndex:
-        DateTSlist.append(str(int(time.mktime(date.timetuple()))))
+    
+    for date in date_index:
+        DateList.append(str(int(time.mktime(date.timetuple()))))
+    
     if timeST=='N':
-        for string in DateTSlist:
-            value=int(string)
+        for string in DateList:
+            value = int(string)
             DateList.append(datetime.utcfromtimestamp(value).strftime('%Y-%m-%d'))
-        return DateList
-    return DateTSlist
+
+    return DateList
 
 
-#return a list containing the elements of list_1 (bigger one) non included in list_2 (smaller one)
+
+# return a list containing the elements of list_1 (bigger one) non included in list_2 (smaller one)
+
 def Diff(list_1, list_2): 
+    
     return (list(set(list_1) - set(list_2))) 
 
-#return a sorted array of the size of reference_array.
+
+
+# return a sorted array of the size of reference_array.
 # if there are more element in ref array, broken_array is filled with the missing elements
 # broken_array HAS TO BE smaller than reference vector
 # default sorting is in ascending way, if descending is needed specify versus='desc'
-def fill_time_array(broken_array, reference_array,versus='asc'):
-    difference=Diff(reference_array,broken_array)
+
+def fill_time_array(broken_array, ref_array, versus = 'asc'):
+    
+    difference = Diff(ref_array, broken_array)
+    
     for element in difference:
         broken_array.add(element)
-    broken_array=list(broken_array)
-    if versus=='desc':
-        broken_array.sort(reverse=True)
+    broken_array = list(broken_array)
+    
+    if versus == 'desc':
+        broken_array.sort(reverse = True)
+    
     else:
         broken_array.sort()
+    
     return broken_array
 
-# def find_index(broken_array, reference_array):
-#     difference=Diff(reference_array,broken_array)
-#     broken_array=np.array(broken_array)
-#     reference_array=np.array(reference_array)
-#     index=[]
-#     for element in difference:
-#         i, = np.where(reference_array==element)
-#         index.append(i)
-#     return np.array(index)
+
 
 # function that given a list of item, find the items and relative indexes in another list/vector
 # if one or more items in list_to_find are not included in where_to_find the fucntion simply go ahead
 # the return matrix have items as first column and index as second column
+
 def find_index(list_to_find, where_to_find):
     list_to_find=np.array(list_to_find)
     where_to_find=np.array(where_to_find)
@@ -101,10 +123,13 @@ def find_index(list_to_find, where_to_find):
     indexed_item=indexed_item[indexed_item[:,0].argsort()]
     return indexed_item
 
+
+
 #given a matrix (where_to_lookup), a date reference array and, broken date array with missing date
 #function returns a matrix where the first column contains the list of date that broken array miss
 #the second column contains the relative weighted variations between T and T-1, the third column contains the T volume
 # specified by "position"(4=close price, 5= volume in crypto, 6=volume in pair)
+
 def substitute_finder(broken_array, reference_array, where_to_lookup,position):
     missing_item=Diff(reference_array,broken_array)
     indexed_list=find_index(missing_item, where_to_lookup[:,1])
@@ -122,8 +147,11 @@ def substitute_finder(broken_array, reference_array, where_to_lookup,position):
     volume_matrix=np.column_stack((indexed_list[:,0],volumes))
     return variation_matrix, volume_matrix
 
+
+
 # given a list of items (what_to_insert) and the index position that the items should have in where_to_insert
 #the function returns where_to_insert updated with the missing items
+
 def insert_items(index_list, what_to_insert, where_to_insert):
     index_list=np.array(index_list)
     what_to_insert=np.array(what_to_insert)
@@ -133,8 +161,10 @@ def insert_items(index_list, what_to_insert, where_to_insert):
     return where_to_insert
 
 
+
 #given an index list containing the index value of certain items as founded in where_to_insert
 #the function return an array of element in position i-1, searched in where_to_insert
+
 def find_previous(index_list, where_to_insert):
     index_list=np.array(index_list)
     where_to_insert=np.array(where_to_insert)
@@ -145,12 +175,12 @@ def find_previous(index_list, where_to_insert):
 
 
 
-
 # function takes as input a matrix with missing values referred to specific exchange, crypto and pair
 # reference_array is the array of date of the period of interest, info_position can be: 
 #(4=close price, 5= volume in crypto, 6=volume in pair)
 # based on the info_pos choice the function returns a fixed vector that contain also the values obtained as volume weighted average
 # (of close price, volume crypto or volume in pair) of the daily variations of every exchange in the crypto+pair 
+
 def fix_missing(broken_matrix, reference_array, cryptocurrency, exchange, pair, info_position):
     broken_array=broken_matrix[:,0]
     ccy_pair=cryptocurrency+pair
@@ -179,17 +209,12 @@ def fix_missing(broken_matrix, reference_array, cryptocurrency, exchange, pair, 
 
 
 
-
-
-# for i, element in enumerate(eur):
-#     eur[i]=datetime.strptime(str(datetime.fromtimestamp(eur[i]))[:10], '%Y-%m-%d').strftime('%d/%m/%y')
-
-
 #function takes a .json file from Cryptowatch API and transforms it into a matrix
 # the matrix has the headers : ['Time' ,'Open',	'High',	'Low',	'Close',""+Crypto+" Volume" , ""+Pair+" Volume"]
 #if the downloaded file does not have results the function returns an empty array
 #note that the "time" column contains value in timestamp format
 #86400 is the daily frequency in seconds
+
 def json_to_matrix(file_path, Crypto='',Pair=''):
     raw_json=pd.read_json(file_path)
     if Crypto == '':
