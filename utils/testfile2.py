8ca7030e6b95270d8648ca07ee22505a6b41c3e6
mpichini1 <marcello.pichini@gmail.com>
2020-01-10 11:54:45 +0100

    readme

--- utils/testfile2.py
+++ utils/testfile2.py
@@ -10,22 +10,25 @@ import numpy as np
 # a = 'bitfinex'
 
 
-a=[1,3,4,5,6,2,7]
-b=[2,3,4,5,6,7,8]
-a=np.array(a)
-b=np.array(b)
-c=np.column_stack((a,b))
-c=np.column_stack((c,a))
-i,j=np.where(c==2)
-z=np.where(c==2)
-print(c)
-v=c.sum(axis=1)
-print(v)
-f=c/a[:,None]
-r=c[2,1:]/c[1,1:]
-print(r)
-print(len(c))
-print(c*2)
-t=np.row_stack((a,b))
-print(t)
+# a=[1,3,4,5,6,2,7]
+# b=[2,3,4,5,6,7,8]
+# a=np.array(a)
+# b=np.array(b)
+# c=np.column_stack((a,b))
+# c=np.column_stack((c,a))
+# i,j=np.where(c==2)
+# z=np.where(c==2)
+# print(c)
+# v=c.sum(axis=1)
+# print(v)
+# f=c/a[:,None]
+# r=c[2,1:]/c[1,1:]
+# print(r)
+# print(len(c))
+# print(c*2)
+# t=np.row_stack((a,b))
+# print(t)
 
+a = data_download.CW_data_reader('coinbase_pro', 'btcusd', '01-01-2020')
+
+print(a)
