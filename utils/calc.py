63c2d2f61425544b6165ebdaf06d371076df5e99
mpichini1 <marcello.pichini@gmail.com>
2020-01-13 12:42:34 +0100

    adding computing dates functions

--- utils/calc.py
+++ utils/calc.py
@@ -1,6 +1,7 @@
 import numpy as np
 import datetime
 from datetime import *
+from datetime import date, datetime, timedelta
 
 
 # Return the Initial Divisor for the index. It identifies the position of the initial date in the Curr_Volume_Matrix. 
@@ -116,6 +117,59 @@ def emwa_currencies_volume(Curr_Volume_Matrix, moving_average_period = 90):
 
     return emwa_gen
 
+# this function return a range of data in a certain interval. Is usefull because is it possible  to iterate on it.
+
+def perdelta(start, end, delta):
+
+    curr = start
+    while curr < end:
+        yield curr
+        curr += delta
+
+    return 
+
+
+# this function generates an array cointaing the date of the start of each quarter from the start of the index till the current day.
+def start_q(perdelta): 
+
+    start_q = np.array([])
+    for result in perdelta(datetime.date(2016, 1, 1), datetime.date.today(), relativedelta(months=3)):
+        start_q = np.append(start_q, result)
+    
+    return start_q
+
+
+# this fuction checks if a day is a business day or not.
+# returns a bool value 
+
+def is_business_day(date):
+    
+    return bool(len(pd.bdate_range(date, date)))
+
+
+def board_meeting_day():
+    
+    board_day = np.array([])
+
+    for result in perdelta(datetime.date(2015, 12, 21), datetime.date.today(), relativedelta(months=3)):
+        # checks if the date generated is a business day, if yes the date is added to the array
+        # if not, the fuction go 1 day back and makes the same check. If the condition is still not respected, is going back another day.
+        # the if statement goes back two days maximum: Sunday and Saturday.
+        if is_business_day(result) == True:
+            board_day = np.append(board_day, result)
+        elif is_business_day(result-datetime.timedelta(days= 1)) == True:
+            board_day = np.append(board_day, result-datetime.timedelta(days= 1))
+        else:
+           board_day = np.append(board_day result-datetime.timedelta(days= 2))
+    
+    return board_day
+
+
+# returns an array with the day before of each board meeting.
+
+def day_before_board():
+
+    before_board_day = board_meeting_day() - datetime.timedelta(days= 1)
 
 # function returns a matrix with the weights that every currency should have
 # takes as input the currecny matrix of volume and the logic matrix 
@@ -127,6 +181,17 @@ def EMWA_weights(Curr_Volume_Matrix, logic_matrix):
     EMWA_weights_matrix = (emwa_volume_curr * logic_matrix) / total_EMWA_volume[:, None]
 
     return EMWA_weights_matrix
+
+
+#function that returns a matrix containing the starting weights of the index for the Quarter.
+# takes as input the EMWA_weights_matrix selecting the relevant date.
+
+def quarter_weight(EMWA_weights):
+
+
+
+
+
 
 
 # function returns a matrix with the same number and order of column of the Curr Price Matrix containing
@@ -308,6 +373,7 @@ def Curr_logic_matrix2(perc_emwa_per_curr):
     curr_logic_matrix2 = np.column_stack((logic_row,curr_logic_matrix2))
 
     return curr_logic_matrix2
+
 
 
 # function takes as input:
--- utils/testfile2.py
+++ utils/testfile2.py
@@ -12,11 +12,8 @@ import numpy as np
 # a = 'bitfinex'
 
 
-<<<<<<< HEAD
 # a=[1000,3,4,5,6,2,7]
-=======
 # a=[1,3,4,5,6,2,7]
->>>>>>> 3b92781b0b89ee0ccfe078526a0afdcb85bb5e79
 # b=[2,3,4,5,6,7,8]
 # a=np.array(a)
 # b=np.array(b)
@@ -34,66 +31,7 @@ import numpy as np
 # print(c*2)
 # t=np.row_stack((a,b))
 # print(t)
-<<<<<<< HEAD
-# y=[1,2,3,4,5,6,7,8]
-# y=np.array(y)
-# print(y)
-# x=np.append(t,y)
-# print(x)
-
-# print(len(c[0]))
-# gg=np.sum(c,axis=1)
-# print(gg/gg.sum())
-# Start_Period='2020-01-02'
-# End_Period='2020-01-05'
-# key_currency_vector= ['USD','JPY']
-# a=data_download.ECB_rates_extractor(key_currency_vector,Start_Period)
-# print(a)
-
-# frequency='D'
-# currency_denominat='EUR'
-# typeRates='SP00'
-# series_variation='A'
-# entrypoint = 'https://sdw-wsrest.ecb.europa.eu/service/' 
-# resource = 'data'           
-# flowRef ='EXR'
-# parameters = {
-#     'startPeriod': Start_Period, 
-#     'endPeriod': End_Period    
-# }
-# Exchange_Rate_List=pd.DataFrame()
-# pd.options.mode.chained_assignment = None
-# for i,currency in enumerate(key_currency_vector):
-#     key= frequency+'.'+currency+'.'+currency_denominat+'.'+typeRates+'.'+series_variation
-#     request_url = entrypoint + resource + '/'+ flowRef + '/' + key
-#     response = requests.get(request_url, params=parameters, headers={'Accept': 'text/csv'})
-#     try:
-#         Data_Frame = pd.read_csv(io.StringIO(response.text))
-#         Main_Data_Frame = Data_Frame.filter(['TIME_PERIOD', 'OBS_VALUE','CURRENCY','CURRENCY_DENOM'], axis=1)
-#     except:
-#         Main_Data_Frame=[]
-
-# print(request_url)
-# print(Main_Data_Frame)
-# a=[]
-# a.append(2)
-# print(a)
-# a.append(None)
-# print(a)
-# a=np.array(a)
-# print(a)
-# if a[1]== None:
-#     print('yes')
-# b=np.column_stack((a,a))
-# c=np.column_stack((a,b))
-# print(len(c[:,0]))
-# print(c)
-
-s=data_setup.date_reformat('12-01-1991','-')
-print(s)
-=======
 
 a = data_download.CW_data_reader('coinbase-pro', 'btcusdct', '01/01/2020')
 
 print(a)
->>>>>>> 3b92781b0b89ee0ccfe078526a0afdcb85bb5e79
