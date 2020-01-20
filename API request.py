0f8cec2543ecdf5881891da1de024b56b1d857f9
mpichini1 <marcello.pichini@gmail.com>
2020-01-20 13:27:41 +0100

    update api request

--- API request.py
+++ API request.py
@@ -4,29 +4,43 @@
 #start date = ISO 8601
 #end date = ISO 8601
 #granularity = in seconds. Ex 86400 = 1 day
-#this api give back 300 responses max each request.
+#this api gives back 300 responses max for each request.
+
 import requests
 from requests import get
 from datetime import datetime
 import pandas as pd
+import numpy as np
 
-date = datetime.strptime( '11-11-2017','%m-%d-%Y')
-start_date = date.isoformat()
-
-date2 = datetime.strptime( '11-13-2017','%m-%d-%Y')
-end_date = date2.isoformat()
-
+def date_gen():
+    start = datetime(2016, 1, 1)
+    end = datetime(2019, 12, 1)
+    pace = end
+    delta = timedelta(days=50)
+    while(start < end):
+        pace = start + delta
+        yield (str(start.isoformat()), str(pace.isoformat()))
+        start = pace
+        
+d_gen = date_gen()
+#print(d_gen)
 crypto = 'BTC'#, 'XRP', 'LTC', '']
-fiat = 'EUR'#], 'GBP', 'USDC', 'USD']
+fiat = 'USD'#], 'GBP', 'USDC', 'USD']
 
 granularity = '86400'
 
+alo = np.array([])
+ciao = np.array([])
 
-entrypoint = 'https://api.pro.coinbase.com/products/'
-key = crypto+'-'+fiat+'/candles?start='+start_date+'&end='+end_date+'&granularity='+granularity
-request_url = entrypoint + key
-
-response = requests.get(request_url)
-response = response.json()
-header = ['time', 'low', 'high', 'open', 'close', 'volume']
-dataframe = pd.DataFrame(response, columns = header)
+for start,pace in d_gen:
+    print(start)
+    print(pace)
+    entrypoint = 'https://api.pro.coinbase.com/products/'
+    key = crypto+'-'+fiat+'/candles?start='+start+'&end='+pace+'&granularity='+granularity
+    request_url = entrypoint + key
+    response = requests.get(request_url)
+    response= response.json()
+    
+header = ['Time', 'low', 'high', 'open', 'Close Price', 'Crypto Volume']
+dataframe = pd.DataFrame(alo, columns = header)
+dataframe = dataframe.drop(columns = ['open', 'high', 'low'])
