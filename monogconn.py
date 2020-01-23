550db4f5bb7c58b5201f790294bb7e8cb9059054
mpichini1 <marcello.pichini@gmail.com>
2020-01-23 13:52:07 +0100

    mongoconnect

--- monogconn.py
+++ monogconn.py
@@ -1,6 +1,6 @@
 connection = MongoClient('localhost', 27017)
 db = connection.index
-db.tweets.ensure_index("id", unique=True, dropDups=True)
+db.rawdata.ensure_index("id", unique=True, dropDups=True)
 collection = db.rawdata
 
  collection.save(x)
