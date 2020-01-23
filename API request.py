e485316dc4f3ed2fba4f00f76a4b847bbd673d83
mpichini1 <marcello.pichini@gmail.com>
2020-01-23 11:27:32 +0100

    api request update

--- API request.py
+++ API request.py
@@ -9,7 +9,9 @@
 #granularity = in seconds. Ex 86400 = 1 day
 #this api gives back 300 responses max for each request.
 ##for the moment it is just downloading one cryptocurr.
+#-------------------------------------------------------------------------------------------------------
 
+#BTC USD import
 import requests
 from requests import get
 from datetime import *
@@ -57,5 +59,61 @@ for start,pace in d_gen:
     # coinbase-pro allows 4 calls per seconds maximum so we use a sleep 
     sleep(0.25)
 
-Coinbase = dataframe.drop(columns = ['open', 'high', 'low'])          
+Coinbase = dataframe.drop(columns = ['open', 'high', 'low'])       
+
     
+#############################################################àààà
+#questa da il problema della chiave
+import requests
+from requests import get
+from datetime import *
+import pandas as pd
+import numpy as np
+from time import sleep
+
+def date_gen():
+    start = datetime(2016, 1, 1)
+    stop = datetime(2019, 12, 1)
+    pace = stop
+    delta = timedelta(days=49)
+    while(start < stop):
+        end = start + delta
+        yield (str(start.isoformat()), str(end.isoformat()))
+        start = end
+        
+d_gen = date_gen()
+#print(d_gen)
+crypto =  ['BTC']
+curr = ['USD']#, 'USDC', 'USD']
+
+granularity = '86400'
+
+df = np.array([])
+header = ['Time', 'low', 'high', 'open', 'Close Price', 'Crypto Volume']
+d = {}
+
+for assets in crypto:
+    for fiat in curr:
+        for start,stop in d_gen:
+            sleep(0.35)
+            entrypoint = 'https://api.pro.coinbase.com/products'
+            key = assets+"-"+fiat+"/candles?start="+start+"&end="+end+"&granularity="+granularity
+            request_url = entrypoint + key
+            response = requests.get(request_url)
+            response= response.json()
+            response = np.array(response)
+            print(response)
+            try :
+                if df.size == 0 :
+                    header = ['Time', 'low', 'high', 'open', 'Close Price', 'Crypto Volume']
+                    dataframe = pd.DataFrame(response, columns = header)
+                    alo = np.append(alo,response)
+                else:
+                    dataframe = dataframe.append(pd.DataFrame(response, columns = header))
+                    
+            except:        
+                continue
+
+            d["df_{}_{}".format(assets, fiat)] = dataframe
+            
+#Coinbase = dataframe.drop(columns = ['open', 'high', 'low'])          
