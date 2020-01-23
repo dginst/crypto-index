55aa5dc824116cd879321b894a5aa3f870f3875c
mpichini1 <marcello.pichini@gmail.com>
2020-01-23 16:30:39 +0100

    api request other exchanges added

--- API request.py
+++ API request.py
@@ -1,6 +1,7 @@
 #####################################################################################################
 ################################ COINBASE - PRO #####################################################
 #####################################################################################################
+
 #REST API request for coinbase-pro exchange. 
 #It requires:
 #crypto pair = crypto-fiat (ex: BTC - USD)
@@ -21,7 +22,7 @@ from time import sleep
 
 
 #questa da il problema della chiave
-def date_gen(Start_Date, End_Date, delta):
+def date_gen_isoformat(Start_Date, End_Date, delta):
     start = datetime.strptime(Start_Date,'%m-%d-%Y') 
     stop = datetime.strptime(End_Date,'%m-%d-%Y') 
     delta = timedelta(days=delta)
@@ -33,18 +34,31 @@ def date_gen(Start_Date, End_Date, delta):
         yield (str(pace.isoformat()), str(end.isoformat()))
         pace = end + timedelta(days = 1)
 
+def date_gen_timestamp(Start_Date, End_Date, delta):
+    start = datetime.strptime(Start_Date,'%m-%d-%Y') 
+    stop = datetime.strptime(End_Date,'%m-%d-%Y') 
+    delta = timedelta(days=delta)
+    pace = start
+    while (pace < stop):
+        end = pace + delta
+        if end > stop:
+            end = stop
+        yield (str(pace.timestamp()), str(end.timestamp()))
+        pace = end + timedelta(days = 1)
+
+
 
 crypto =  ['BTC']
 curr = ['USD']#, 'USDC', 'USD']
 
 def Coinbase_API(Start_Date, End_Date, Crypto, Fiat, granularity = '86400', ):
 
-    date_object = date_gen('01-01-2019','12-01-2019',49)
+    date_object = date_gen_isoformat('01-01-2019','12-01-2019',49)
     df = np.array([])
     header = ['Time', 'low', 'high', 'open', 'Close Price', 'Crypto Volume']
     d = {}
 
-    for assets in Crypto:
+    for asset in Crypto:
 
         for fiat in Fiat:
             
@@ -65,9 +79,146 @@ def Coinbase_API(Start_Date, End_Date, Crypto, Fiat, granularity = '86400', ):
                     df = np.row_stack((df, response))
                 
                
-    #             d["df_{}_{}".format(assets, fiat)] = dataframe ##perche???????
-
+    #non dovrebbe essere all'interno del ciclo?
     Coinbase_df = pd.DataFrame(df, columns=header)        
     Coinbase_df = Coinbase_df.drop(columns = ['open', 'high', 'low'])      
 
     return Coinbase_df    
+
+
+
+    #####################################################################################################
+    ################################      KRAKEN    #####################################################
+    #####################################################################################################
+
+    #The REST API OHLC endpoint only provides a limited amount of historical data, specifically 720 data points of the requested interval.
+    #unfortunally the since option seems to not work. so here the date_gen fuction is useless
+
+    def kraken_API(Start_Date, End_Date, Crypto, Fiat, interval  = '1440', ):
+
+    df = np.array([])
+    header = ['Time', 'low', 'high', 'open', 'Close Price', 'Crypto Volume']
+    d = {}
+
+    for asset in Crypto:
+
+        for fiat in Fiat:
+            
+            entrypoint = 'https://api.kraken.com/0/public/OHLC?'
+            key = 'pair='+assets+fiat+'&interval='+interval
+            request_url = entrypoint + key
+
+            response = requests.get(request_url)
+            sleep(0.25)
+
+            response = response.json()
+
+            if df.size == 0 :
+                df = np.array(response)
+            else:
+                df = np.row_stack((df, response))
+            
+               
+
+    Coinbase_df = pd.DataFrame(df, columns=header)        
+    Coinbase_df = Coinbase_df.drop(columns = ['open', 'high', 'low'])      
+
+    return kraken_df    
+
+
+
+
+    #####################################################################################################
+    ################################      BITTREX    #####################################################
+    #####################################################################################################
+
+    # https://bittrex.github.io/api/v3 api actually not working for historical data
+
+
+
+
+    #####################################################################################################
+    ################################    Poloniex    #####################################################
+    #####################################################################################################
+
+
+
+
+
+    def Poloniex_API(Start_Date, End_Date, Crypto, Fiat, period = '86400', ):
+
+    date_object = date_gen_timestamp('01-01-2019','12-01-2019',49)
+    df = np.array([])
+    header = ['Time', 'low', 'high', 'open', 'Close Price', 'Crypto Volume']
+    d = {}
+
+    for asset in Crypto:
+
+        for fiat in Fiat:
+            
+            for start,stop in date_object:
+
+                entrypoint = 'https://poloniex.com/public?command=returnChartData&currencyPair='
+                key = asset+'_'fiat+'&start='+start+'&end='+stop+'&period='+period
+                request_url = entrypoint + key
+
+                response = requests.get(request_url)
+                sleep(0.25)
+
+                response = response.json()
+
+                if df.size == 0 :
+                    df = np.array(response)
+                else:
+                    #errore qui
+                    df = np.row_stack((df, response))
+                
+               
+    #non dovrebbe essere all'interno del ciclo?
+    Coinbase_df = pd.DataFrame(df, columns=header)        
+    Coinbase_df = Coinbase_df.drop(columns = ['open', 'high', 'low'])      
+
+    return Poloniex_df    
+
+
+
+    #####################################################################################################
+    ################################     itBit      #####################################################
+    #####################################################################################################
+
+
+
+    def itBit_API(Start_Date, End_Date, Crypto, Fiat, period = '86400', ):
+
+    date_object = date_gen_timestamp('01-01-2019','12-01-2019',49)
+    df = np.array([])
+    header = ['Time', 'low', 'high', 'open', 'Close Price', 'Crypto Volume']
+    d = {}
+
+    for asset in Crypto:
+
+        for fiat in Fiat:
+            
+            for start,stop in date_object:
+
+                entrypoint = 'https://poloniex.com/public?command=returnChartData&currencyPair='
+                key = asset+'_'fiat+'&start='+start+'&end='+stop+'&period='+period
+                request_url = entrypoint + key
+
+                response = requests.get(request_url)
+                sleep(0.25)
+
+                response = response.json()
+
+                if df.size == 0 :
+                    df = np.array(response)
+                else:
+                    #errore qui
+                    df = np.row_stack((df, response))
+                
+               
+    #non dovrebbe essere all'interno del ciclo?
+    Coinbase_df = pd.DataFrame(df, columns=header)        
+    Coinbase_df = Coinbase_df.drop(columns = ['open', 'high', 'low'])      
+
+    return Poloniex_df    
