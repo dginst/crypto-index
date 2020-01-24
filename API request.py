5b6f9bfd9a58ff50b9ddb2bfbe43aaefa1823da1
FraSkoll <fcodega2@gmail.com>
2020-01-24 11:12:48 +0100

    changes in API request

--- API request.py
+++ API request.py
@@ -1,18 +1,3 @@
-#####################################################################################################
-################################ COINBASE - PRO #####################################################
-#####################################################################################################
-
-#REST API request for coinbase-pro exchange. 
-#It requires:
-#crypto pair = crypto-fiat (ex: BTC - USD)
-#start date = ISO 8601
-#end date = ISO 8601
-#granularity = in seconds. Ex 86400 = 1 day
-#this api gives back 300 responses max for each request.
-##for the moment it is just downloading one cryptocurr.
-#-------------------------------------------------------------------------------------------------------
-
-#BTC USD import
 import requests
 from requests import get
 from datetime import *
@@ -21,11 +6,19 @@ import numpy as np
 from time import sleep
 
 
-#questa da il problema della chiave
+# function takes as input Start Date, End Date (both string in mm-dd-yyyy format) and delta (numeric)
+# then creates an object containing multiple couples of date in ISO format with "delta"  days between them
+# for a total length defined by start date and end date. If the last delta is bigger than end date, then 
+# end date is taken as last date of the object
+
 def date_gen_isoformat(Start_Date, End_Date, delta):
+     
+    # converting string to date format
     start = datetime.strptime(Start_Date,'%m-%d-%Y') 
     stop = datetime.strptime(End_Date,'%m-%d-%Y') 
+    # define delta as days
     delta = timedelta(days=delta)
+
     pace = start
     if Start_Date != End_Date:
 
@@ -35,41 +28,80 @@ def date_gen_isoformat(Start_Date, End_Date, delta):
 
             if end > stop:
                 end = stop
+
             yield (str(pace.isoformat()), str(end.isoformat()))
             pace = end + timedelta(days = 1)
 
+    # if start date == end date, then a couple containing the same date is returned
     else:
         
         yield (str(start.isoformat()), str(stop.isoformat()))
 
 
 
+# function takes as input Start Date, End Date (both string in mm-dd-yyyy format) and delta (numeric)
+# then creates an object containing multiple couples of date in TIMESTAMP format with "delta"  days between them
+# for a total length defined by start date and end date. If the last delta is bigger than end date, then 
+# end date is taken as last date of the object
+
 def date_gen_timestamp(Start_Date, End_Date, delta):
+
+    # converting string to date format
     start = datetime.strptime(Start_Date,'%m-%d-%Y') 
     stop = datetime.strptime(End_Date,'%m-%d-%Y') 
+    # define delta as days
     delta = timedelta(days=delta)
+
     pace = start
-    while (pace < stop):
-        end = pace + delta
-        if end > stop:
-            end = stop
-        yield (str(pace.timestamp()), str(end.timestamp()))
-        pace = end + timedelta(days = 1)
+    if Start_Date != End_Date:
+
+        while (pace < stop):
+
+            end = pace + delta
+            if end > stop:
+
+                end = stop
+
+            yield (str(pace.timestamp()), str(end.timestamp()))
+            pace = end + timedelta(days = 1)
+    
+    # if start date == end date, then a couple containing the same date is returned
+    else:
+
+        yield (str(start.isoformat()), str(stop.isoformat()))
 
 
+#####################################################################################################
+################################ COINBASE - PRO #####################################################
+#####################################################################################################
 
-crypto =  ['BTC']
-curr = ['USD']#, 'USDC', 'USD']
+# REST API request for coinbase-pro exchange. 
+# It requires:
+# crypto pair = crypto-fiat (ex: BTC - USD)
+# start date = ISO 8601
+# end date = ISO 8601
+# granularity = in seconds. Ex 86400 = 1 day
+# this api gives back 300 responses max for each request.
 
-def Coinbase_API(Crypto, Fiat, Start_Date, End_Date = None, granularity = '86400', ):
+#-------------------------------------------------------------------------------------------------------
+
+# function that given Crypto (ex. BTC), Fiat (ex. USD), start DAte and End Date 
+# retrieve the Historical Series from start date to End Date of the definend Crypto + Fiat
+# on the Coinbase Pro Exchange
+# the output will be displayed in three columns containing: 'Time' in timestamp format,
+# 'Close Price' in "Fiat" currency, 'Crypto Volume' in "Crypto" amount
+
+def Coinbase_API(Crypto, Fiat, Start_Date, End_Date = None, granularity = '86400'):
+
+    Crypto = Crypto.upper()
+    Fiat = Fiat.upper()
 
     if End_Date == None:
         End_Date = datetime.now().strftime('%m-%d-%Y')
    
-    date_object = date_gen(Start_Date, End_Date, 49)
+    date_object = date_gen_isoformat(Start_Date, End_Date, 49)
     df = np.array([])
     header = ['Time', 'low', 'high', 'open', 'Close Price', 'Crypto Volume']
-    # d = {}
 
     for asset in Crypto:
 
@@ -78,7 +110,7 @@ def Coinbase_API(Crypto, Fiat, Start_Date, End_Date = None, granularity = '86400
             for start,stop in date_object:
 
                 entrypoint = 'https://api.pro.coinbase.com/products/'
-                key = assets+"-"+fiat+"/candles?start="+start+"&end="+stop+"&granularity="+granularity
+                key = asset+"-"+fiat+"/candles?start="+start+"&end="+stop+"&granularity="+granularity
                 request_url = entrypoint + key
 
                 response = requests.get(request_url)
@@ -100,25 +132,33 @@ def Coinbase_API(Crypto, Fiat, Start_Date, End_Date = None, granularity = '86400
 
 
 
-    #####################################################################################################
-    ################################      KRAKEN    #####################################################
-    #####################################################################################################
+#####################################################################################################
+################################      KRAKEN    #####################################################
+#####################################################################################################
 
-    #The REST API OHLC endpoint only provides a limited amount of historical data, specifically 720 data points of the requested interval.
-    #unfortunally the since option seems to not work. so here the date_gen fuction is useless
+# The REST API OHLC endpoint only provides a limited amount of historical data, specifically
+# 720 data points of the requested interval.
+# unfortunally the since option seems to not work. so here the date_gen fuction is useless
 
-    def kraken_API(Start_Date, End_Date, Crypto, Fiat, interval  = '1440', ):
+#-------------------------------------------------------------------------------------------------------
+
+# function that given Crypto (ex. BTC), Fiat (ex. USD), start DAte and End Date 
+# retrieve the Historical Series from start date to End Date of the definend Crypto + Fiat
+# on the Kraken Exchange
+# the output will be displayed in three columns containing: 'Time' in timestamp format,
+# 'Close Price' in "Fiat" currency, 'Crypto Volume' in "Crypto" amount
+
+def kraken_API(Start_Date, End_Date, Crypto, Fiat, interval  = '1440', ):
 
     df = np.array([])
-    header = ['Time', 'low', 'high', 'open', 'Close Price', 'Crypto Volume']
-    d = {}
+    header = ['Time', 'open', 'high', 'low', 'Crypto Price', 'vwap', 'Crypto Volume', 'count']
 
     for asset in Crypto:
 
         for fiat in Fiat:
             
             entrypoint = 'https://api.kraken.com/0/public/OHLC?'
-            key = 'pair='+assets+fiat+'&interval='+interval
+            key = 'pair='+asset+fiat+'&interval='+interval
             request_url = entrypoint + key
 
             response = requests.get(request_url)
@@ -131,42 +171,43 @@ def Coinbase_API(Crypto, Fiat, Start_Date, End_Date = None, granularity = '86400
             else:
                 df = np.row_stack((df, response))
             
-               
+            
 
-    Coinbase_df = pd.DataFrame(df, columns=header)        
-    Coinbase_df = Coinbase_df.drop(columns = ['open', 'high', 'low'])      
+    Kraken_df = pd.DataFrame(df, columns=header)        
+    Kraken_df = Kraken_df.drop(columns = ['open', 'high', 'low', 'vwap', 'count'])      
 
     return Kraken_df    
 
 
 
 
-    #####################################################################################################
-    ################################     BITTREX    #####################################################
-    #####################################################################################################
+#####################################################################################################
+################################     BITTREX    #####################################################
+#####################################################################################################
 
-    # https://bittrex.github.io/api/v3 api actually not working for historical data
-
-
-
-
-    #####################################################################################################
-    ################################    POLONIEX    #####################################################
-    #####################################################################################################
+# https://bittrex.github.io/api/v3 api actually not working for historical data
 
 
 
 
+#####################################################################################################
+################################    POLONIEX    #####################################################
+#####################################################################################################
 
-    def Poloniex_API(Start_Date, End_Date, Crypto, Fiat, period = '86400', ):
+# function that given Crypto (ex. BTC), Fiat (ex. USD), start DAte and End Date 
+# retrieve the Historical Series from start date to End Date of the definend Crypto + Fiat
+# on the Poloniex Exchange
+# the output will be displayed in three columns containing: 'Time' in timestamp format,
+# 'Close Price' in "Fiat" currency, 'Crypto Volume' in "Crypto" amount
+
+def Poloniex_API(Start_Date, End_Date, Crypto, Fiat, period = '86400'):
 
     if End_Date == None:
         End_Date = datetime.now().strftime('%m-%d-%Y')
    
-    date_object = date_gen(Start_Date, End_Date, 49)
+    date_object = date_gen_timestamp(Start_Date, End_Date, 49)
     df = np.array([])
-    header = ['Time', 'low', 'high', 'open', 'Close Price', 'Crypto Volume']
-    d = {}
+    header = ['Time', 'open', 'high', 'low', 'Crypto Price', 'Crypto Volume', 'volume_usd', 'vwap']
 
     for asset in Crypto:
 
@@ -175,7 +216,7 @@ def Coinbase_API(Crypto, Fiat, Start_Date, End_Date = None, granularity = '86400
             for start,stop in date_object:
 
                 entrypoint = 'https://poloniex.com/public?command=returnChartData&currencyPair='
-                key = asset+'_'fiat+'&start='+start+'&end='+stop+'&period='+period
+                key = asset+'_'+fiat+'&start='+start+'&end='+stop+'&period='+period
                 request_url = entrypoint + key
 
                 response = requests.get(request_url)
@@ -183,83 +224,81 @@ def Coinbase_API(Crypto, Fiat, Start_Date, End_Date = None, granularity = '86400
 
                 response = response.json()
 
-                if df.size == 0 :
+                if df.size == 0:
+
                     df = np.array(response)
+
                 else:
-                    #errore qui
+
                     df = np.row_stack((df, response))
                 
                
-    #non dovrebbe essere all'interno del ciclo?
-    Coinbase_df = pd.DataFrame(df, columns=header)        
-    Coinbase_df = Coinbase_df.drop(columns = ['open', 'high', 'low'])      
+
+    Poloniex_df = pd.DataFrame(df, columns=header)        
+    Poloniex_df = Poloniex_df.drop(columns = ['open', 'high', 'low', 'vwap', 'volume usd'])      
 
     return Poloniex_df    
 
 
 
-    #####################################################################################################
-    ################################      ITBIT     #####################################################
-    #####################################################################################################
+#####################################################################################################
+################################      ITBIT     #####################################################
+#####################################################################################################
 
-    # https://www.itbit.com/api api actually not working for historical data 
-
-
-
-
-    #####################################################################################################
-    ################################    BITFLYER    #####################################################
-    #####################################################################################################
-
-    # https://lightning.bitflyer.com/docs?lang=en api actually not working for historical data 
+# https://www.itbit.com/api api actually not working for historical data 
 
 
 
+#####################################################################################################
+################################    BITFLYER    #####################################################
+#####################################################################################################
 
-    #####################################################################################################
-    ################################     GEMINI     #####################################################
-    #####################################################################################################
-    
-    #gives back data from 1 year before, that's bad
+# https://lightning.bitflyer.com/docs?lang=en api actually not working for historical data 
 
 
-    def Gemini_API(Start_Date, End_Date, Crypto, Fiat, time_frame = '1day', ):
+
+
+#####################################################################################################
+################################     GEMINI     #####################################################
+#####################################################################################################
+
+# returns data just starting from 1 year back
+
+
+def Gemini_API(Start_Date, End_Date, Crypto, Fiat, time_frame = '1day'):
 
     if End_Date == None:
         End_Date = datetime.now().strftime('%m-%d-%Y')
    
-    date_object = date_gen(Start_Date, End_Date, 49)
     df = np.array([])
+
     header = ['Time', 'low', 'high', 'open', 'Close Price', 'Crypto Volume']
-    d = {}
 
     for asset in Crypto:
 
         for fiat in Fiat:
             
-            for start,stop in date_object:
+            entrypoint = 'https://api.gemini.com/v2'
+            key = '/candles/'+asset+fiat+'/'+time_frame
+            request_url = entrypoint + key
 
-                entrypoint = 'https://api.gemini.com/v2'
-                key = '/candles/'+Crypto+Fiat+'/'+time_frame
-                request_url = entrypoint + key
+            response = requests.get(request_url)
+            sleep(0.25)
 
-                response = requests.get(request_url)
-                sleep(0.25)
+            response = response.json()
 
-                response = response.json()
+            if df.size == 0 :
 
-                if df.size == 0 :
-                    df = np.array(response)
-                else:
-                    #errore qui
-                    df = np.row_stack((df, response))
+                df = np.array(response)
+            else:
+
+                df = np.row_stack((df, response))
                 
                
-    #non dovrebbe essere all'interno del ciclo?
-    Coinbase_df = pd.DataFrame(df, columns=header)        
-    Coinbase_df = Coinbase_df.drop(columns = ['open', 'high', 'low'])      
+    Gemini_df = pd.DataFrame(df, columns=header)        
+    Gemini_df = Gemini_df.drop(columns = ['open', 'high', 'low'])      
 
-    return Poloniex_df    
+    return Gemini_df    
 
 
 
