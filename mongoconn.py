4423ecfa0b047d92bd5fb6bdeec4dfe3f0d1e90f
mpichini1 <marcello.pichini@gmail.com>
2020-01-27 10:10:15 +0100

    update

--- mongoconn.py
+++ mongoconn.py
@@ -1,6 +1,6 @@
 #this code block has to be put before all the download functions.
 import API_request
-
+from utils.API_request import date_gen_isoformat
 from pymongo import MongoClient
 
 #connecting to mongo in local
@@ -14,30 +14,31 @@ collection = db.rawdata
 #-----------------------------------------------------------------------------------------------------------
 
 # i don't know why, it stops after first cicle BTCUSD
+    
+def Coinbase_API(Start_Date='01-01-2017', End_Date='12-01-2019', Crypto = ['ETH', 'BTC'], Fiat=['USD','EUR'], granularity = '86400', ):
 
-def Coinbase_API(Start_Date='01-01-2019', End_Date='12-01-2019', Crypto = ['BTC'], Fiat=['USD'], granularity = '86400', ):
-
-    date_object = API_request.date_gen_isoformat('01-01-2019','12-01-2019',49)
+    date_object = date_gen_isoformat(Start_Date,End_Date,49)
     df = np.array([])
     header = ['Time', 'low', 'high', 'open', 'Close Price', 'Crypto Volume']
-    
-    
+    d = {}
+
     for asset in Crypto:
-
+        print(asset)
         for fiat in Fiat:
-            
+            print(fiat)    
             for start,stop in date_object:
-
+                print(start,stop)
+                
                 entrypoint = 'https://api.pro.coinbase.com/products/'
-                key = assets+"-"+fiat+"/candles?start="+start+"&end="+stop+"&granularity="+granularity
+                key = asset+"-"+fiat+"/candles?start="+start+"&end="+stop+"&granularity="+granularity
+                
                 request_url = entrypoint + key
-
+                print(request_url)
                 response = requests.get(request_url)
-                sleep(0.25)
-
+                sleep(0.3)
                 response = response.json()
-               
-               # it saves each day in mongo. To do that a for loop is utilized.  
+                
+                #print(response)
                 for i in range(len(response)):
                     
                     r = response
@@ -50,9 +51,9 @@ def Coinbase_API(Start_Date='01-01-2019', End_Date='12-01-2019', Crypto = ['BTC'
                     Close_Price = r[i][4]
                     Crypto_Volume = r[i][5]
 
-    
+
                     rawdata = { 'Exchange' : Exchange, 'Pair' : Pair, 'Time':Time, 'Low':Low, 'High':High, 'Open':Open, 'Close Price':Close_Price, 'Crypto Volume':Crypto_Volume}
-    
+
                     collection.insert_one(rawdata)
 
             
