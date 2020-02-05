c8775e1c1cf10ae6a6c4e900718bc63dd6c85a8d
mpichini1 <marcello.pichini@gmail.com>
2020-02-05 17:32:45 +0100

    changes

--- TEST_hostorical with mongo.py
+++ TEST_hostorical with mongo.py
@@ -23,13 +23,102 @@ collection_clean = db.cleandata
 collection_ECB_raw = db.ecb_raw
 
 
+crypto = ['btc', 'eth']
+pair_array = ['jpy', 'gbp', 'usd'] #, 'eur', 'cad', 'usdt', 'usdc'
+
+
+Crypto_Asset = ['BTC', 'ETH']
+
+Exchanges = ['kraken', 'bitflyer', 'poloniex' ] #, 'bitstamp','bittrex','coinbase-pro','gemini']
+start_date = '01-01-2020'
+today = datetime.now().strftime('%Y-%m-%d')
+today_TS = int(datetime.strptime(today,'%Y-%m-%d').timestamp()) + 3600
+
+reference_date_vector = np.array(data_setup.date_array_gen(start_date, timeST='Y'))
+
+##
+# define the array containing the rebalance start date
+rebalance_start_date = calc.start_q('01-01-2016')
+rebalance_stop_date = calc.stop_q(rebalance_start_date)
+
+board_date = calc.board_meeting_day()
+board_date_eve = calc.day_before_board()
+##
+
+# potrei dire che si attiva il calcolo del rebalance solo quando (today in rebalance start date o end date
+# quidi solo in quella occasione cambia il vettore di 0 e 1
+
+
+print(reference_date_vector)
+
+key= ['USD', 'GBP', 'CAD', 'JPY']
+rates = data_setup.ECB_setup(key, '2020-01-01', today, timeST='Y')
+#print(rates)
 
 
 
+Crypto_Asset_Prices = np.matrix([])
+Crypto_Asset_Volume = np.matrix([])
+
+for CryptoA in Crypto_Asset:
+    print(CryptoA)
+    currencypair_array = []
+    Exchange_Price = np.matrix([])
+    Exchange_Volume = np.matrix([])
+    Ex_PriceVol = np.matrix([])
+    first_logic_matrix = np.matrix([])
+    new_first_logic_matrix = np.matrix([])
+    for i in pair_array:
+        currencypair_array.append(CryptoA.lower() + i)
+
+    for exchange in Exchanges:
+        
+        # initialize the matrices that will contain the data related to all currencypair for the single exchange
+        Ccy_Pair_PriceVolume = np.matrix([])
+        Ccy_Pair_Volume = np.matrix([])
+        Ccy_Pair_Price = np.matrix([])
+        
+        for cp in currencypair_array:
+
+            crypto = cp[:3]
+            pair = cp[3:]
+            # create the matrix for the single currency_pair connecting to CryptoWatch website
+            mongo.CW_data_reader(exchange, cp, start_date)
+            matrix = mongo.query_raw_mongo(cp)
+
+            
+
+            # creates the to-be matrix of the cp assigning the reference date vector as first column
+            cp_matrix = reference_date_vector
+
+            # checking if the matrix is not empty
+            if data_setup.Check_null(matrix) == False:
+
+                # checking if the matrix has missing data and if ever fixing it
+                if matrix.shape[0] != reference_date_vector.size:
+
+                    matrix= data_setup.fix_missing(matrix, exchange, crypto, pair, start_date)
 
 
+                # changing the "fiat" values into USD (Close Price and Volume)
+                matrix= data_setup.CW_data_setup(matrix, rates, pair)
+                cp_matrix = matrix.to_numpy()
 
+                # then retrieves the wanted data 
+                priceXvolume = cp_matrix[:,1] * cp_matrix[:,2]
+                volume = cp_matrix[:,2]
+                price = cp_matrix[:,1]
+                priceXvolume = cp_matrix[:,1] * cp_matrix[:,2]
+                volume = cp_matrix[:,2]
+                price = cp_matrix[:,1]
 
+                # every "cp" the for loop adds a column in the matrices referred to the single "exchange"
+                if Ccy_Pair_PriceVolume.size == 0:
+                    Ccy_Pair_PriceVolume = priceXvolume
+                    Ccy_Pair_Volume = volume
+                else:
+                    Ccy_Pair_PriceVolume = np.column_stack((Ccy_Pair_PriceVolume, priceXvolume))
+                    Ccy_Pair_Volume = np.column_stack((Ccy_Pair_Volume, volume))
 
 
         # computing the volume weighted average price of the single exchange
