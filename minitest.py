ce5904e111d91525ac5e1167eaf759edf130b804
FraSkoll <fcodega2@gmail.com>
2020-02-13 11:53:09 +0100

    change

--- minitest.py
+++ minitest.py
@@ -3,113 +3,13 @@ from datetime import datetime
 import utils.data_setup as ds
 import numpy as np
 import pandas as pd
-import datetime
+from datetime import datetime
 from datetime import *
-from datetime import date, datetime, timedelta
+import time
+#from datetime import date, datetime, timedelta
 from dateutil.relativedelta import relativedelta
 
-# def perdelta(start, end, delta):
 
-#     curr = start
-#     while curr < end:
-#         yield curr
-#         curr += delta
-
-# #####################################
-
-# def is_business_day(date):
-    
-#     return bool(len(pd.bdate_range(date, date)))
-
-
-# #####################################################
-
-# def previuos_business_day(date):
-
-#     while is_business_day(date) == False:
-#         date = date - timedelta(days = 1)
-    
-#     return date
-
-
-# ########################################################
-
-# def start_q(start_date = '01-01-2016', stop_date = None, delta = None, timeST = 'Y', lag_adj = 3600): 
-
-#     start_date = datetime.strptime(start_date,'%m-%d-%Y')
-
-#     if delta == None:
-
-#         delta = relativedelta(months = 3)
-#     if stop_date == None:
-
-#         stop_date = datetime.now().strftime('%m-%d-%Y')
-#         stop_date = datetime.strptime(stop_date,'%m-%d-%Y')
-
-#     start_day_arr = np.array([])
-
-#     for result in perdelta(start_date, stop_date, delta):
-#         if timeST == 'Y':
-#             result = int(result.timestamp())
-#             result = result + lag_adj
-#         else:
-#             result = result.strftime('%m-%d-%Y')
-        
-#         start_day_arr = np.append(start_day_arr, result)
-
-#     return start_day_arr
-
-# #################################################################
-
-# def board_meeting_day(start_date = '12-21-2015', stop_date = None, delta = None, timeST = 'Y', lag_adj = 3600):
-    
-#     start_date = datetime.strptime(start_date,'%m-%d-%Y')
-
-#     if delta == None:
-
-#         delta = relativedelta(months = 3)
-#     if stop_date == None:
-
-#         stop_date = datetime.now().strftime('%m-%d-%Y')
-#         stop_date = datetime.strptime(stop_date,'%m-%d-%Y')
-
-#     board_day = np.array([])
-
-#     for result in perdelta(start_date, stop_date, delta):
-#         # checks if the date generated is a business day, if yes the date is added to the array
-#         # if not, the fuction go 1 day back and makes the same check. If the condition is still not respected, is going back another day.
-#         # the if statement goes back two days maximum: Sunday and Saturday.
-#         result = previuos_business_day(result) 
-
-#         if timeST == 'Y':
-
-#             result = int(result.timestamp())
-#             result = result + lag_adj
-#         else:
-
-#             result = result.strftime('%m-%d-%Y')
-
-#         board_day = np.append(board_day, result)
-
-
-#     return board_day
-
-# ###############################################################
-# # b = datetime(2016, 1, 1)
-# # print(b)
-
-# # for i in start_q('01-01-2016'):
-# #     print(i)
-
-# # print(is_business_day('01-01-2020'))
-
-# # start = '02-03-2020'
-# # start_date = datetime.strptime(start,'%m-%d-%Y')
-# # print(is_business_day(start_date))
-# # print(previuos_business_day(start_date))
-# # print(board_meeting_day())
-
-# ###########################aggiungere descrizione e mettere in calc
 # def quarterly_period(start_date = '01-01-2016', stop_date = None):
 
 #     start_date = datetime.strptime(start_date,'%m-%d-%Y')
@@ -183,35 +83,83 @@ from dateutil.relativedelta import relativedelta
 # print(d)
 
 
-a = [1,2,3,4,5,6,7,8,9,10]
-b=np.array(a)
-v=b*100
-head = ['ciao', 'test']
-c = np.column_stack((b,b))
-asd = ['ciao', 'test']
-d = pd.DataFrame(c,columns= head)
-g = d<3
+# a = [1,2,3,4,5,6,7,8,9,10]
+# b=np.array(a)
+# v=b*100
+# head = ['ciao', 'test']
+# c = np.column_stack((b,b))
+# asd = ['ciao', 'test']
+# d = pd.DataFrame(c,columns= head)
+# g = d<3
+# # print(d)
+# # print(g)
+# # g= g*1
+# # print(g)
+# # g= g.drop(columns=['A'])
+
+# g[asd]= d[head]
+# print(g)
+# d['Time']=v
+# g['Time']=v
+# print(g)
+# # pd.options.mode.chained_assignment = None  # default='warn'
+# # g[asd][g['Time'].between(300, 800, inclusive = True)] = d[head][d['Time'] == 100]
+# x=d.loc[d.Time == 100, head]
+# print(x)
+# g.loc[g.Time.between(300, 800, inclusive = True), asd] = np.array(x)
+# # d[head][d['Time'] == 100]
 # print(d)
 # print(g)
-# g= g*1
-# print(g)
-# g= g.drop(columns=['A'])
+# new = ['ciao', 'test', 'Time']
+# p = (g>1)*1
+# print(p)
+# print(p*g)
+# x = ['Ciao','fra']
+# reference_date_array = [1,2,3,4,5,6,7,8,9]
+# reference_date_array = np.array(reference_date_array)
+# reshaped_matrix = pd.DataFrame(reference_date_array, columns = ['Time'])
+# for el in x:
+#     reshaped_matrix[el] = np.zeros(len(reference_date_array))
+# print(reshaped_matrix)
 
-g[asd]= d[head]
-print(g)
-d['Time']=v
-g['Time']=v
-print(g)
-# pd.options.mode.chained_assignment = None  # default='warn'
-# g[asd][g['Time'].between(300, 800, inclusive = True)] = d[head][d['Time'] == 100]
-x=d.loc[d.Time == 100, head]
-print(x)
-g.loc[g.Time.between(300, 800, inclusive = True), asd] = np.array(x)
-# d[head][d['Time'] == 100]
-print(d)
-print(g)
-new = ['ciao', 'test', 'Time']
-p = (g>1)*1
-print(p)
-print(p*g)
 
+
+# def timestamp_gen(start_date, end_date = None,  EoD = 'Y'):
+
+#     if end_date == None:
+#         end_date = datetime.now().strftime('%m-%d-%Y')
+
+#     end_date = datetime.strptime(end_date,'%m-%d-%Y')
+#     end = int(time.mktime(end_date.timetuple()))
+#     end = end + 3600
+#     start = datetime.strptime(start_date,'%m-%d-%Y')
+#     start = int(time.mktime(start.timetuple()))
+#     start = start + 3600
+
+#     array = np.array([start])
+#     date = start
+#     print(end)
+   
+#     while date < end:
+#         date = date + 86400
+#         array = np.append(array, date)
+    
+#     array = array[:len(array) - 1]
+    
+#     return array
+
+# x = timestamp_gen('01-01-2019') 
+# print(x)
+
+ts = 1459465200
+
+#ts = ts - 3600
+l = datetime.utcfromtimestamp(ts)
+print(ts)
+print(l)
+print(l.hour)
+if datetime.utcfromtimestamp(ts).hour == 23:
+    ts = ts+3600
+
+print(ts)
+print(datetime.utcfromtimestamp(ts))
