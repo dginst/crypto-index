afd1e0787a1e59e65c3ee3733e1bac6fb44147e9
mpichini1 <marcello.pichini@gmail.com>
2020-04-03 11:54:09 +0200

    eliminating some tests

--- cron-test.py
+++ cron-test.py
@@ -20,23 +20,23 @@ collection_krakentraw = db.krakentraw
 
 ########################## COINBASE
 
-# assets
-assets = ['BTC', 'ETH']
-assets1 = ['LTC', 'BCH', 'ETC']
-assets2 = ['XLM', 'EOS', 'XRP']
-assets3 = ['ZEC']
+# # assets
+# assets = ['BTC', 'ETH']
+# assets1 = ['LTC', 'BCH', 'ETC']
+# assets2 = ['XLM', 'EOS', 'XRP']
+# assets3 = ['ZEC']
 
-# fiat
-fiat = ['EUR', 'USD', 'GBP', 'USDC']
-fiat1 = ['EUR', 'USD', 'GBP']
-fiat2 = ['EUR', 'USD']
-fiat3 = ['USDC']
+# # fiat
+# fiat = ['EUR', 'USD', 'GBP', 'USDC']
+# fiat1 = ['EUR', 'USD', 'GBP']
+# fiat2 = ['EUR', 'USD']
+# fiat3 = ['USDC']
 
-call = [api.coinbase_ticker(Crypto, Fiat, collection_coinbasetraw) for Crypto in assets  for Fiat in fiat]
-call = [api.coinbase_ticker(Crypto, Fiat, collection_coinbasetraw) for Crypto in assets1  for Fiat in fiat1]
-call = [api.coinbase_ticker(Crypto, Fiat, collection_coinbasetraw) for Crypto in assets2  for Fiat in fiat2]
-call = [api.coinbase_ticker(Crypto, Fiat, collection_coinbasetraw) for Crypto in assets3  for Fiat in fiat3]
-coinbase = time.time()
+# call = [api.coinbase_ticker(Crypto, Fiat, collection_coinbasetraw) for Crypto in assets  for Fiat in fiat]
+# call = [api.coinbase_ticker(Crypto, Fiat, collection_coinbasetraw) for Crypto in assets1  for Fiat in fiat1]
+# call = [api.coinbase_ticker(Crypto, Fiat, collection_coinbasetraw) for Crypto in assets2  for Fiat in fiat2]
+# call = [api.coinbase_ticker(Crypto, Fiat, collection_coinbasetraw) for Crypto in assets3  for Fiat in fiat3]
+# coinbase = time.time()
         
 
 ######################### KRAKEN
@@ -58,38 +58,38 @@ kraken = time.time()
 
 ######################## itbit
 
-assets1 = ['BTC', 'ETH']
-fiat1 = ['EUR', 'USD']
+# assets1 = ['BTC', 'ETH']
+# fiat1 = ['EUR', 'USD']
 
-for Crypto in assets1:
-    for Fiat in  fiat1:
+# for Crypto in assets1:
+#     for Fiat in  fiat1:
 
-        api.itbit_ticker(Crypto, Fiat, collection_itbittraw)
+#         api.itbit_ticker(Crypto, Fiat, collection_itbittraw)
 
-itbit = time.time()
+# itbit = time.time()
 
 ######################## bitstamp
 
-assets2 = ['BTC', 'ETH','XRP','LTC','BCH']
-fiat1 = ['EUR', 'USD']
+# assets2 = ['BTC', 'ETH','XRP','LTC','BCH']
+# fiat1 = ['EUR', 'USD']
 
-for Crypto in assets2:
-    for Fiat in fiat1:
+# for Crypto in assets2:
+#     for Fiat in fiat1:
 
-        api.bitstamp_ticker(Crypto, Fiat, collection_bitstamptraw)
+#         api.bitstamp_ticker(Crypto, Fiat, collection_bitstamptraw)
 
-bitstamp = time.time()
+# bitstamp = time.time()
 ######################## gemini
 
-assets3 = ['BTC', 'ETH','LTC','BCH','ZEC']
-fiat2 = ['USD']
+# assets3 = ['BTC', 'ETH','LTC','BCH','ZEC']
+# fiat2 = ['USD']
 
-for Crypto in assets3:
-    for Fiat in fiat2:
+# for Crypto in assets3:
+#     for Fiat in fiat2:
 
-        api.gemini_ticker(Crypto,Fiat, collection_geminitraw)
+#         api.gemini_ticker(Crypto,Fiat, collection_geminitraw)
 
-gemini = time.time()
+# gemini = time.time()
 ######################## bittrex
 
 assets = ['BTC', 'ETH','BCH', 'BSV']
@@ -106,31 +106,31 @@ call =[api.bittrex_ticker(Crypto, Fiat, collection_bittrextraw)  for Crypto in a
 bittrex = time.time()
 ######################## poloniex
 
-assets = ['BTC', 'ETH','BCHABC', 'BCHSV', 'LTC','XRP','ZEC', 'EOS', 'ETC', 'STR', 'XMR']
-stbc = ['USDC', 'USDT']
+# assets = ['BTC', 'ETH','BCHABC', 'BCHSV', 'LTC','XRP','ZEC', 'EOS', 'ETC', 'STR', 'XMR']
+# stbc = ['USDC', 'USDT']
 
-call =[api.poloniex_ticker(Crypto,Fiat, collection_poloniextraw)  for Crypto in assets for Fiat in stbc]
+# call =[api.poloniex_ticker(Crypto,Fiat, collection_poloniextraw)  for Crypto in assets for Fiat in stbc]
 
-poloniex = time.time()
+# poloniex = time.time()
 ###################### bitflyer
 
-assets1 = ['BTC']
-assets2 = ['ETH']
-fiat1 = ['EUR', 'USD', 'JPY']
-fiat2 = ['JPY']
+# assets1 = ['BTC']
+# assets2 = ['ETH']
+# fiat1 = ['EUR', 'USD', 'JPY']
+# fiat2 = ['JPY']
 
-call =[api.bitflyer_ticker(Crypto, Fiat, collection_bitflyertraw)  for Crypto in assets1 for Fiat in fiat1]
-call =[api.bitflyer_ticker(Crypto, Fiat, collection_bitflyertraw)  for Crypto in assets2 for Fiat in fiat2]        
+# call =[api.bitflyer_ticker(Crypto, Fiat, collection_bitflyertraw)  for Crypto in assets1 for Fiat in fiat1]
+# call =[api.bitflyer_ticker(Crypto, Fiat, collection_bitflyertraw)  for Crypto in assets2 for Fiat in fiat2]        
         
-bitflyer = time.time()
+# bitflyer = time.time()
 end = time.time()
 
-print("This script took: {} minutes".format(float(coinbase-start)))
-print("This script took: {} minutes".format(float(kraken-coinbase)))
-print("This script took: {} minutes".format(float(itbit-kraken)))
-print("This script took: {} minutes".format(float(bitstamp-itbit)))
-print("This script took: {} minutes".format(float(gemini-bitstamp)))
-print("This script took: {} minutes".format(float(bittrex-gemini)))
-print("This script took: {} minutes".format(float(poloniex-bittrex)))
-print("This script took: {} minutes".format(float(bitflyer-poloniex)))
-print("This script took: {} minutes".format(float(end-start)))
+# print("This script took: {} minutes".format(float(coinbase-start)))
+# print("This script took: {} minutes".format(float(kraken-coinbase)))
+# print("This script took: {} minutes".format(float(itbit-kraken)))
+# print("This script took: {} minutes".format(float(bitstamp-itbit)))
+# print("This script took: {} minutes".format(float(gemini-bitstamp)))
+# print("This script took: {} minutes".format(float(bittrex-gemini)))
+# print("This script took: {} minutes".format(float(poloniex-bittrex)))
+# print("This script took: {} minutes".format(float(bitflyer-poloniex)))
+# print("This script took: {} minutes".format(float(end-start)))
