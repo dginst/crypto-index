5d14bbda9233edf3ef474f17df545a2315a11e1a
mpichini1 <marcello.pichini@gmail.com>
2020-04-15 14:20:07 +0200

    sh file update

--- linux_initial_setup.sh
+++ linux_initial_setup.sh
@@ -14,7 +14,7 @@ echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongod
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 
-#Start MongoDB and check status
+#Start MongoDB 
 sudo systemctl start mongod
 #$ sudo systemctl status mongod  # you can run this command to check tha mongodb status, then ctrl + c to exit
 
