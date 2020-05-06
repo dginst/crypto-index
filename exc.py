d64d0ada17fd302731122fa46952c888ca81a45d
mpichini1 <marcello.pichini@gmail.com>
2020-05-06 10:30:13 +0200

    updating conversion script

--- exc.py
+++ exc.py
@@ -41,15 +41,6 @@ today_TS = int(datetime.strptime(today,'%Y-%m-%d').timestamp()) + 3600
 # the date are displayed as timestamp and each day refers to 12:00 am UTC
 reference_date_vector = data_setup.timestamp_gen(start_date)
 
-# pair arrat without USD (no need of conversion)
-pair_array = ['usd', 'gbp', 'eur', 'cad', 'jpy', 'usdt', 'usdc']
-# pair complete = ['gbp', 'usd', 'cad', 'jpy', 'eur'] 
-Crypto_Asset = ['ETH', 'BTC', 'LTC', 'BCH', 'XRP', 'XLM', 'ADA', 'ZEC', 'XMR', 'EOS', 'BSV', 'ETC'] 
-# crypto complete ['ETH', 'BTC', 'LTC', 'BCH', 'XRP', 'XLM', 'ADA', 'ZEC', 'XMR', 'EOS', 'BSV', 'ETC']
-Exchanges = [ 'coinbase-pro', 'poloniex', 'bitstamp', 'gemini', 'bittrex', 'kraken', 'bitflyer']
-# exchange complete = ['coinbase-pro', 'poloniex', 'bitstamp', 'gemini', 'bittrex', 'kraken', 'bitflyer']
-ciao = 'ethusdt'
-print(ciao[3:])
 ####################################### setup MongoDB connection ###################################
 
 #connecting to mongo in local
@@ -72,62 +63,42 @@ db = "index"
 collection_data = "CW_cleandata"
 collection_rates = "ecb_clean"
 
+# querying the data from mongo
 matrix_rate = mongo.query_mongo2(db, collection_rates)
 matrix_data = mongo.query_mongo2(db, collection_data)
-######################################################
-############# alternativa #########################
-# leave out USD pair
-matrix_data['fiat'] = matrix_data['Pair'].str[3:]
-fiat_to_conv = ['gbp', 'eur', 'cad', 'jpy']
-fiat_no_conv = ['usd', 'usdt', 'usdc']
-matrix_data = matrix_data.loc[matrix_data['fiat'].isin(fiat_to_conv)]
-matrix_no_conv = matrix_data.loc[matrix_data['fiat'].isin(fiat_no_conv)]
 
-matrix_rate['key'] = matrix_rate['Currency'].str[:3].lower() + matrix_rate['Time']
-matrix_data['key'] = matrix_data['Pair'].str[3:] + matrix_rate['Time']
+# creating subsets only for the crypto/usd, crypto/usdc and crypto/usdt  pairs that don't need conversiont and storing them in mongo
+first_conv_data = matrix_data.loc[matrix_data['Pair'].str[3:] == 'usd']
 
-new_df = matrix_data.merge(matrix_rate, on = 'key',  how = 'left')
-new_df['Close Price'] = new_df['Close Price'] / new_df['Rate']
-print(new_df)
-print(new_df.info())
-# rearrange matrix
-new_df = new_df.drop(columns = ['key'])
-final_matrix = new_df.append(matrix_no_conv)
-final_matrix = final_matrix.drop(columns = ['fiat'])
-###################################################
-# queryng the two dataframe
+first_conv_data = matrix_data.loc[matrix_data['Pair'].str[3:] == 'usdc']
 
-matrix_rate['a'] = [x[:3].lower() for x in matrix_rate['Currency'] ]
-print(matrix_rate['a'])
-matrix_data['a'] = [x[3:].lower() for x in matrix_data['Pair'] ]
-print(matrix_data['a'])
-df = pd.merge(matrix_data,matrix_rate, on = ['Time','a'])
+first_conv_data = matrix_data.loc[matrix_data['Pair'].str[3:] == 'usdt']
 
-df = df['Close Price'] * df['Rate']
+
+
+# creating an equal column in both datasets for merging
+
+matrix_rate['fiat'] = [x[:3].lower() for x in matrix_rate['Currency'] ]
+matrix_data['fiat'] = [x[3:].lower() for x in matrix_data['Pair'] ]
+
+
+# merging the dataset
+
+df = pd.merge(matrix_data,matrix_rate, on = ['Time','fiat'])
+
+# converting the prices in usd
+
+df['Close Price'] = df['Close Price'] * df['Rate']
+
+# subsetting the dataset with only the relevant columns
+
+df = df[['Time', 'Close Price', 'Crypto Volume', 'Standard Date']]
+
+#storing the prices in mongo 
 
 print(df.info())
+print(df.head())
 ##########################################
-# first_conv_data = matrix_data.loc[matrix_data['Pair'].str[3:] == 'usd']
-# print(first_conv_data.info())
-# first_conv_data = matrix_data.loc[matrix_data['Pair'].str[3:] == 'usdc']
-# print(first_conv_data.info())
-# first_conv_data = matrix_data.loc[matrix_data['Pair'].str[3:] == 'usdt']
-# print(first_conv_data.info())
-
-
-
-
-# df['Currency'] = df['Currency'].apply(lambda x: x.lower())
-
-# df = df.loc[df['Pair'].str[3:] == df['Currency'].str[:3]]
-
-# df['Close Price'] = df['Close Price'] * df['Rate']
-
-
-        
-   
-# df['Close Price'] = df['Close Price'] * df['Rate']
-
 
 
 
