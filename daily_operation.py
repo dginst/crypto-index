7d7f8e8c81a591af267433cecbc29169608994c1
Codega <fcodega@deloitte.it>
2020-06-08 11:01:54 +0200

    repo cleaning

--- daily_operation.py
+++
@@ -1,8 +0,0 @@
-import ECB_daily
-
-ECB_daily()
-# os.system('ECB_daily.py')
-# execfile('ECB_hist_setup.py')
-# execfile('CW_daily_donload.py')
-# execfile('CW_daily_setup.py')
-# execfile('CW_daily_conversion.py')
