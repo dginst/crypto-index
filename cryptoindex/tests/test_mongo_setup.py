d2be84cafb03064cf7d31678f67bf996b0545fd6
Codega <fcodega@deloitte.it>
2020-06-19 11:47:30 +0200

    new test

--- cryptoindex/tests/test_mongo_setup.py
+++ cryptoindex/tests/test_mongo_setup.py
@@ -1,8 +1,9 @@
 # third party packages
 from os import path
-
+import json
 import pandas as pd
 from pymongo import MongoClient
+from bson.json_util import loads
 
 # internal import
 from cryptoindex.mongo_setup import query_mongo
@@ -11,8 +12,25 @@ from cryptoindex.mongo_setup import query_mongo
 connection = MongoClient("localhost", 27017)
 db = connection.index
 
+# creating the collection for the test
+db.test_mongo.drop()
+
+# creating the empty collection cleandata within the database index
+db.test_mongo.create_index([("id", -1)])
+coll_mongo = db.test_mongo
+
+# defining file path and naming
 filename = "mongo_test.json"
 data_folder = path.join(path.dirname(__file__), "data", filename)
+
+# uploading the file in the test_mongo collection
+
+with open(data_folder) as m:
+
+    data = m.read()
+    file_data = loads(data)
+
+coll_mongo.insert_many(file_data)
 
 
 def test_query_mongo():
@@ -22,12 +40,10 @@ def test_query_mongo():
 
     df_test = df_test.drop(columns=["_id"])
 
-    #df_test.drop("_id", axis=1, inplace=True)
-
     # querying the collection from db
 
     database = "index"
-    collection = "test_api_request"
+    collection = "test_mongo"
 
     df_mongo = query_mongo(database, collection)
 
